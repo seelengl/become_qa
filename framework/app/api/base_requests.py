efcc56ff69147787e53c4362d35d8f187d189949
Iurii Malyk <iurii.malyk@gmail.com>
2022-04-28 03:01:32 +0300

    I exestid but i CAN

--- framework/app/api/base_requests.py
+++ framework/app/api/base_requests.py
@@ -1,16 +1,16 @@
 import requests
-from config.conf import Config
+from config.config import Config
+
 class BaseRequests:
-    """Class for calling HTTP requests"""
+    """Class for colling HTTP requests"""
     def form_url(self, url):
-        """Method to concat basse url and api path"""
+        """method to concat base url and api path"""
         return Config.BASE_URL + url
-    def get(self, path, params):
+    def get(self, path, *args, **kwargs):
         """Reimplementation of GET method"""
         url = self.form_url(path)
-        return requests.get(url, params)
-
-    def post(self, path, params):
+        return requests.get(url, *args, **kwargs)
+    def post(self, path, *args, **kwargs):
         """Reimplementation of POST method"""
         url = self.form_url(path)
-        return requests.post(url, params)
+        return requests.post(url, *args, **kwargs)
