ece0a599db869118de6bd065c095e43b202c616a
sergii-butenko-gl <69191372+sergii-butenko-gl@users.noreply.github.com>
2022-04-28 11:53:24 +0300

    Update test_login.py

--- framework/test/test_login.py
+++ framework/test/test_login.py
@@ -1,11 +1,27 @@
 from app.api.client import ApiClient
+import pytest
+import logging
 
-def test_login():
+logger = logging.get_logger()
+
+
+@pytest.fixture
+def api_session(scope='session'):
     ac = ApiClient()
-    assert ac.login() is not None
-    assert ac.get_folders() is not None
-    assert ac.get_file() is not None
-    assert ac.get_file_count() is not None
-    assert ac.get_file_runs() is not None
-    assert ac.get_file_analysis() is not None
-    assert ac.get_files_artifacts() is not None
+    ac.login()
+    yeild ac
+    
+def test_get_folders(api_session):
+    assert api_session.get_folders() is not None
+    
+def test_get_file(api_session):
+    assert api_session.get_file() is not None
+
+def test_get_file_count():
+    assert api_session.get_file_count() == 22
+    
+    
+    ##assert ac.get_file_runs() is not None
+    ##assert ac.get_file_analysis() is not None
+    ##assert ac.get_files_artifacts() is not None
+    
