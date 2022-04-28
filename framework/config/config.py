b97aa35bae484b8335ecb9cbf9a380652b58801d
seelengl <102672194+seelengl@users.noreply.github.com>
2022-04-28 03:23:27 +0300

    Update and rename conf.py to config.py

--- framework/config/config.py
+++ framework/config/config.py
@@ -3,4 +3,4 @@ import os
 class Config:
     """Class defines all the configuration test framework"""
     BASE_URL='https://app.cosmosid.com'
-    LOGIN_TOKEN= os.environ.get('LOGIN_TOKEN', None)
+    LOGIN_TOKEN= 'Basic bWFpbGludGVzdEB1a3IubmV0Om1haWxpbnRlc3RAdWtyLm5ldA=='
